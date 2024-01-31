f775934127511ec3224a6dc1dfdac5c85b5c9926
liquidliang <ljquan@qq.com>
2024-01-31 11:32:26 +0800

    feat: 优化editor test

--- test/editor.test.ts
+++ test/editor.test.ts
@@ -10,6 +10,7 @@ import {
 } from "vitest";
 import { Window } from "happy-dom";
 import JEditor from "../src/editor";
+import { editorData, imageChild } from "./fixtures/editor.data";
 
 describe("JEditor", () => {
   let window;
@@ -93,94 +94,13 @@ describe("JEditor", () => {
     vi.spyOn(editor, "clear");
     vi.spyOn(editor, "resize");
     vi.spyOn(editor, "createShape")
-    // .mockReturnValue({
-    //   styled: false,
-    //   style: {},
-    // });
     vi.spyOn(editor, "addChild");
-
-    const testData = JSON.stringify({
-      style: { backgroundColor: "red" },
-      children: [
-        {
-          children: [],
-          type: "image",
-          data: {
-            x: 300,
-            left: 300,
-            y: 400,
-            top: 400,
-            width: 100,
-            height: 100,
-            rotation: 0,
-            src: "https://pixijs.com/assets/bunny.png",
-          },
-          style: {
-            bottom: "auto",
-            display: "inline-block",
-            height: 100,
-            left: 300,
-            margin: "0",
-            overflow: "visible",
-            padding: "0",
-            position: "absolute",
-            right: "auto",
-            top: 400,
-            transform: "rotateZ(0rad) scaleX(1) scaleY(1)",
-            width: 100,
-            zIndex: "0",
-          },
-          transform: {
-            translateX: 0,
-            translateY: 0,
-            translateZ: 0,
-            rotateX: 0,
-            rotateY: 0,
-            rotateZ: 0,
-            scaleX: 1,
-            scaleY: 1,
-            scaleZ: 1,
-            skewX: 0,
-            skewY: 0,
-          },
-          id: "a1acb6d480ad4f1b94cc47c04c8d5cd7",
-        },
-        // 更多的孩子对象,如果需要
-      ],
-      type: "",
-      data: {
-        x: 198.5,
-        left: 198.5,
-        y: 252.5,
-        top: 252.5,
-        width: 600,
-        height: 800,
-      },
-      transform: {
-        translateX: 0,
-        translateY: 0,
-        translateZ: 0,
-        rotateX: 0,
-        rotateY: 0,
-        rotateZ: 0,
-        scaleX: 1,
-        scaleY: 1,
-        scaleZ: 1,
-        skewX: 0,
-        skewY: 0,
-      },
-      id: "f8390b94642049f494934be4052fcaf0",
-    });
-
-    editor.fromJSON(testData);
+    editor.fromJSON(editorData);
 
     expect(editor.target.dom.style.backgroundColor).toBe("red");
     expect(editor.clear).toBeCalled();
     expect(editor.resize).toBeCalled();
-    expect(editor.createShape).toBeCalledWith("someType", {
-      type: "someType",
-      otherData: "otherData",
-    });
-    expect(editor.addChild).toBeCalled(); // 做孩子数的断言
+    expect(editor.createShape).toBeCalledWith(imageChild.type, imageChild);
+    expect(editor.addChild).toHaveBeenCalledTimes(1); // 1个children
   });
 });
